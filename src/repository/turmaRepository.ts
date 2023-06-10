80aff1362c342ac2070cdd34f36ec7cd2c9824b8
ICARO SANTOS <icaro.santos@sanar.com>
2023-06-10 14:30:21 -0300

    create endpoint to add student in turma

--- src/repository/turmaRepository.ts
+++ src/repository/turmaRepository.ts
@@ -28,3 +28,36 @@ export const createTurma = async (turma: TCreateTurma) => {
 
 }
 
+
+export const addStudentInTurma = async (studentId: string, turmaId: string) => {
+    const sql = 'INSERT INTO turma_aluno (fk_turma_fk_aluno,fk_cod_turma,fk_cpf_aluno) VALUES (?, ?,?)';
+    return new Promise((resolve, reject) => {
+
+        const pk = studentId.concat(turmaId);
+
+        dbConnection.query(sql, [pk, turmaId, studentId], (error, results: any) => {
+            if (error) {
+
+                if (error?.code == `ER_DUP_ENTRY` && error.message.includes("for key 'PRIMARY'")) {
+                    return reject(new BusinessExceptions("Aluno já cadastrado nessa turma!", "duplicateMatricula", 404));
+                }
+
+                if (error?.code == `ER_NO_REFERENCED_ROW_2` && error.message.includes("FOREIGN KEY (`fk_cpf_aluno`) REFERENCES `aluno` (`cpf`))")) {
+                    return reject(new BusinessExceptions("Aluno não encontrado!", "duplicateMatricula", 404));
+                }
+
+
+                if (error?.code == `ER_NO_REFERENCED_ROW_2` && error.message.includes("FOREIGN KEY (`fk_cod_turma`) REFERENCES `turma` (`codigo`))")) {
+                    return reject(new BusinessExceptions("Turma não encontrada!", "duplicateMatricula", 404));
+                }
+
+                return reject(error);
+            } else {
+                return resolve(results.insertId);
+            }
+        });
+    }).finally(() => {
+        dbConnection.end();
+    })
+}
+
